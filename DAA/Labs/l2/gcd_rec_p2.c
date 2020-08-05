ca26eb58e12dcf4f16e22240cf88c9a319e0db2f
Ganesh Bhandarkar <ganesh@mx>
2020-08-05 17:54:51 +0530

    Lab day 2

---
+++ DAA/Labs/l2/gcd_rec_p2.c
@@ -0,0 +1,23 @@
+/*
+ *  Author: Ganesh Bhandarkar ganesh@mx 1.0 
+ *  Date: 05.08.2020 17:39:47 IST
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+int gcd(int a,int b){
+	if(b==0){
+		return a;
+	}
+	return gcd(a%b,b);
+}
+int main(){
+	srand(time(0));
+	
+	int a,b;scanf("%d%d",&a,&b);
+	int ans = gcd(a,b);
+	printf("%d",ans);	
+	return 0;
+}
